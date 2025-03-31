912bd1a62e57b25b256459c9ced0ca2e5c071008
rolando-cadiz <143847732+rolando-cadiz@users.noreply.github.com>
2025-03-30 22:53:47 -1000

    list contacts page mockup finished in 63 minutes.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'github.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'avatars0.githubusercontent.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'www.ics.hawaii.edu',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
